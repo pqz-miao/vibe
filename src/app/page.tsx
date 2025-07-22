8824b8c4f3fcb55df1b6ee54fea285aedb0c89ea
miao-pqz <1125583676.qq.com>
2025-07-22 21:38:28 +0800

    03: trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-const Page = () => {
+import { Suspense } from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { PageView } from "./page-view";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "prefetch" }));
+
   return (
-    <div>
-      Home Page
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <PageView />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
---
+++ src/trpc/server.ts
@@ -0,0 +1,18 @@
+import "server-only";
+
+import { cache } from "react";
+import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+
+import { createTRPCContext } from "./init";
+import { appRouter } from "./routers/_app";
+import { makeQueryClient } from "./query-client";
+
+export const getQueryClient = cache(makeQueryClient);
+
+export const trpc = createTRPCOptionsProxy({
+    ctx: createTRPCContext,
+    router: appRouter,
+    queryClient: getQueryClient,
+});
+
+export const caller = appRouter.createCaller(createTRPCContext);
