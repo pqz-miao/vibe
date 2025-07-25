4ea466059e5dc1a3818149fc742d55838db75715
miao-pqz <1125583676.qq.com>
2025-07-25 15:09:47 +0800

    05: ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
+import {
+    openai,
+    createAgent
+} from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
-    async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        return { message: `Hello ${event.data.email}!` };
+    async ({ event }) => {
+        const codeAgent = createAgent({
+            name: "code-agent",
+            system: "You are an expert next.js developer. You write readable, maintainable code. You writ simple Next.js & React snippets.",
+            // model: openai({ model: "gpt-4o" }),
+            model: openai({ model: "gpt-4o", baseUrl: "https://yunwu.ai/v1" }),
+        });
+
+        const { output } = await codeAgent.run(
+            `Write the following snippet: ${event.data.value}`,
+        );
+
+        return { output };
     },
 );
