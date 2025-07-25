5aa56ced11eadcc9e02fd29b5e5577433b42b083
miao-pqz <1125583676.qq.com>
2025-07-25 09:10:40 +0800

    04: background jobs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+import { Toaster } from "@/components/ui/sonner";
+
 import { TRPCReactProvider } from "@/trpc/client";
 
 import "./globals.css";
@@ -31,6 +33,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+            <Toaster />
             {children}
         </body>
       </html>
