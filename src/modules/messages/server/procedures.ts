5b00f925bebf7102ff8a3d6b4694424b694c3971
miao-pqz <1125583676.qq.com>
2025-08-22 19:40:38 +0800

    09: projects

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -8,12 +8,16 @@ export const messagesRouter = createTRPCRouter({
     create: baseProcedure
         .input(
             z.object({
-                value: z.string().min(1, "Message is required"),
+                value: z.string()
+                    .min(1, "Prompt is required")
+                    .max(10000, "Prompt is too long"),
+                projectId: z.string().min(1, "Project ID is required"),
             })
         )
         .mutation(async ({ input }) => {
             const message = await prisma.message.create({
                 data: {
+                    projectId: input.projectId,
                     content: input.value,
                     role: "USER",
                     type: "RESULT",
@@ -24,6 +28,7 @@ export const messagesRouter = createTRPCRouter({
                 name: "code-agent/run",
                 data: {
                     value: input.value,
+                    projectId: input.projectId,
                 },
             });
 
