6eba3f357e2bee58a5927f29d88e521bbc220a0d
miao-pqz <1125583676.qq.com>
2025-09-11 16:16:36 +0800

    05: ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -6,14 +6,18 @@ export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event }) => {
-    const summarizer = createAgent({
-      name: "summarizer",
-      system: "You are an expert summarizer. You summarize in 2 words.",
-      model: openai({ model: "gpt-4o", baseUrl: process.env.OPENAI_BASE_URL }),
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: openai({ 
+        model: "gpt-4o", 
+        baseUrl: process.env.OPENAI_BASE_URL,
+        apiKey: process.env.OPENAI_API_KEY,
+      }),
     });
 
-    const { output } = await summarizer.run(
-      `Summarize the following text: ${event.data.value}`
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
     );
     console.log(output);
 
